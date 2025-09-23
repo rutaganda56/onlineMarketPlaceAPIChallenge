79d5fa417179881e4d84da9db81e8c2b54677ef6
Rutaganda <valentinrutaganda04@gmail.com>
2025-09-24 00:54:02 +0300

    Initial commit

---
+++ src/main/java/org/example/onlinemarketplaceapichallenge/OnlineMarketPlaceApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinemarketplaceapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketPlaceApiChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketPlaceApiChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/onlinemarketplaceapichallenge/OnlineMarketPlaceApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinemarketplaceapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketPlaceApiChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
