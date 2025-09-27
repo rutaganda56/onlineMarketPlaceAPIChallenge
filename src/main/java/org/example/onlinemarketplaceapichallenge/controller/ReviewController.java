909e024e885aed3884858fcc27fa6f43c8ad58ef
Rutaganda <valentinrutaganda04@gmail.com>
2025-09-28 00:02:51 +0300

    new commit

--- src/main/java/org/example/onlinemarketplaceapichallenge/controller/ReviewController.java
+++ src/main/java/org/example/onlinemarketplaceapichallenge/controller/ReviewController.java
@@ -1,7 +1,31 @@
 package org.example.onlinemarketplaceapichallenge.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.example.onlinemarketplaceapichallenge.Dto.ReviewDto;
+import org.example.onlinemarketplaceapichallenge.Dto.ReviewResponseDto;
+import org.example.onlinemarketplaceapichallenge.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @GetMapping("/getAllReviews")
+    public List<ReviewResponseDto> getAllReviews() {
+        return reviewService.getAllReviews();
+    }
+    @PostMapping("/createReview")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReviewResponseDto AddReview(@RequestBody ReviewDto reviewDto) {
+        return reviewService.addReview(reviewDto);
+    }
+    @DeleteMapping("/deleteReview/{id}")
+    public void deleteReview(@PathVariable("id") int id) {
+        reviewService.deleteReview(id);
+    }
 }
--- src/main/java/org/example/onlinemarketplaceapichallenge/service/ReviewService.java
+++ src/main/java/org/example/onlinemarketplaceapichallenge/service/ReviewService.java
@@ -1,7 +1,34 @@
 package org.example.onlinemarketplaceapichallenge.service;
 
+import org.example.onlinemarketplaceapichallenge.Dto.ReviewDto;
+import org.example.onlinemarketplaceapichallenge.Dto.ReviewResponseDto;
+import org.example.onlinemarketplaceapichallenge.mapper.ReviewMapper;
+import org.example.onlinemarketplaceapichallenge.mapper.UserMapper;
+import org.example.onlinemarketplaceapichallenge.model.Review;
+import org.example.onlinemarketplaceapichallenge.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepo;
+    @Autowired
+    private ReviewMapper reviewMapper;
+
+    public ReviewResponseDto addReview(ReviewDto reviewDto) {
+        var review = reviewMapper.transformToDto(reviewDto);
+        var savedReview=reviewRepo.save(review);
+        return reviewMapper.transformToResponseDto(savedReview);
+    }
+    public List<ReviewResponseDto> getAllReviews() {
+        return reviewRepo.findAll().stream().map(reviewMapper::transformToResponseDto).collect(Collectors.toList());
+    }
+    public void deleteReview(int reviewId) {
+        reviewRepo.deleteById(reviewId);
+    }
+
 }
